cc94916bab60bfec914b4c5f8fe35f777eefc383
Carrie Ybay <carrieybay@gmail.com>
2017-02-08 14:49:02 -0800

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Format and print data
+ * Return: Prints "with proper grammar, but the outcome is a piece of art,\n"
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
