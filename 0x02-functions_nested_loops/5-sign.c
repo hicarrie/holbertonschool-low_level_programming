a843054a38fb639e9f4b5e00a9eb45d6b32b63b1
Carrie Ybay <carrieybay@gmail.com>
2017-02-14 12:02:56 -0800

    Add 5-sign.c and edit holberton.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_sign - checks sign of number and prints
+ * @n: character to be checked
+ * Return: 0 or 1 or -1
+ */
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
