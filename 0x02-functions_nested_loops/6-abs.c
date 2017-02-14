2f79b9dd8506edbfba6ae3876e76563fe2bedaf6
Carrie Ybay <carrieybay@gmail.com>
2017-02-14 13:31:57 -0800

    Add 6-abs.c and edit holberton.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: integer to be computed
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
