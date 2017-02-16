d9d93cb1ccdfd99777037f51d0d8377b521db29c
Carrie Ybay <carrieybay@gmail.com>
2017-02-15 16:46:42 -0800

    Add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of even fibonacci numbers whose values do not
+ * exceed 4,000,000
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int x, k;
+	unsigned long int i = 1;
+	unsigned long int j = 1;
+	unsigned long int sum = 0;
+
+	for (x = 0; x <= 4000 ; x++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+
+		if (j % 2)
+		{
+			sum = sum + j;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
