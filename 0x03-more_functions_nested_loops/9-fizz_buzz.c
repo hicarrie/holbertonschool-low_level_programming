fccce29fcefe51739f7872ffff1985ad8cd11f82
Carrie Ybay <carrieybay@gmail.com>
2017-02-16 14:21:55 -0800

    Edit 9-fizz_buzz.c

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
+	        if (i % 3 == 0)
 			printf("Fizz");
-		else if (i % 5 == 0)
+	        if (i % 5 == 0)
 			printf("Buzz");
-		else
+	        if ((i % 3 != 0) && (i % 5 != 0))
 			printf("%d", i);
 		printf(" ");
 	}
