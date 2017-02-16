e55bce4985bb4551b6f2948d4b35104a2b9a0b34
Carrie Ybay <carrieybay@gmail.com>
2017-02-16 09:37:12 -0800

    Add 1-isdigit.c and edit holberton.h

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: character to be checked
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
