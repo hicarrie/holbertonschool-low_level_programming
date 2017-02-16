6510faae52073d5f92a0e375e688356e7ee1f3e7
Carrie Ybay <carrieybay@gmail.com>
2017-02-16 09:33:58 -0800

    Add 0-isupper.c and holberton.h

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c: character to be checked
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
