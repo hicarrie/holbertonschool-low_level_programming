1edcef371f63a469402a27b5ec88e22019ee8183
Carrie Ybay <carrieybay@gmail.com>
2017-02-20 15:07:40 -0800

    Add 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int x;
+	unsigned long int i = 1;
+	unsigned long int j = 1;
+	unsigned long int k;
+
+	printf("%lu, ", i);
+
+	for (x = 0; x <= 96; x++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+
+		if (x != 96)
+			printf("%lu, ", j);
+		else
+			printf("%lu\n", j);
+	}
+	return (0);
+}
