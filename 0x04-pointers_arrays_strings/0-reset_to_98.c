4e34426ef872bc7a86dbaead1684b82657fee755
Carrie Ybay <carrieybay@gmail.com>
2017-02-20 15:21:56 -0800

    Add 0-reset_to_98.c, holberton.h, and _putchar.c

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes pointer to int as a parameter and updates value it
+ * points to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
