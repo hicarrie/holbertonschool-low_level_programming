3f568c235fa3d68758d8dab1579bcf2384db134a
Carrie Ybay <carrieybay@gmail.com>
2017-02-20 17:10:16 -0800

    Add 3-puts.c and edit holberton.h

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - prints a string, followed by a newline, to stdout
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
