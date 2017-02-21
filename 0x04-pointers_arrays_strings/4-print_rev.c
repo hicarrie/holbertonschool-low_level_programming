06b153e1aa352c98265debc1d95bcaf28e9482b7
Carrie Ybay <carrieybay@gmail.com>
2017-02-21 13:20:38 -0800

    Edit 4-print_rev.c

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 
 /**
+ * print_rev - prints string in reverse followed by a newline
+ * @s: string to be printed
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j = _strlen(s);
+
+	for (i = j; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+/**
  * _strlen - returns the length of a string
  * @s: string to be evaluated
  * Return: length of string
@@ -15,21 +32,4 @@ int _strlen(char *s)
 	}
 
 	return (i);
-}
-
-/**
- * print_rev - prints string in reverse followed by a newline
- * @s: string to be printed
- * Return: void
- */
-void print_rev(char *s)
-{
-	int i;
-	int j = _strlen(s);
-
-	for (i = j; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar(10);
 }
