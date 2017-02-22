2dcc13840f6c496a9f9c63f0cde8a8f4fe021b12
Carrie Ybay <carrieybay@gmail.com>
2017-02-21 17:15:59 -0800

    Edit 7-puts_half.c

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -8,34 +8,22 @@
 void puts_half(char *str)
 {
 	int i;
-	int j = _strlen(str);
-
-	if (j % 2 == 0)
-		i = j / 2;
-	else
-		i = (j - 1) / 2;
-
-	while (i < j)
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
-}
-
-/**
- * _strlen - returns the length of a string
- * @str: string to be evaluated
- * Return: length of string
- */
-int _strlen(char *str)
-{
-	int i = 0;
+	int j;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
 
-	return (i);
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
 }
