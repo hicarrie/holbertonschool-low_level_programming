43e142dca3f1701ef342467d5b3cd606212a8117
Carrie Ybay <carrieybay@gmail.com>
2017-02-22 08:45:06 -0800

    Add 9-strcpy.c

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copies string pointed to by src to the buffer pointed to dest
+ * @dest: string destination
+ * @src: string source
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j = _strlen(src);
+
+	for (i = 0; i <= j; i++)
+		dest[i] = src[i];
+
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
