0a5db471dbede610e0720ab2c8f029b3b2efb25f
Carrie Ybay <carrieybay@gmail.com>
2017-02-23 12:07:32 -0800

    Add 2-strncpy.c and edit holberton.h

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - copies string pointed to by src to the buffer pointed to dest
+ * @dest: string destination
+ * @src: string source
+ * @n: number of bytes to be used from src
+ * Return: the pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
