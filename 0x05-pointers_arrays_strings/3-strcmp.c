9d2aae58174c3443283bae476797d7ade4e13604
Carrie Ybay <carrieybay@gmail.com>
2017-02-23 17:20:39 -0800

    Edit 3-strcmp.c

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,30 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = _strlen(s1);
-	int j = _strlen(s2);
-
-	if (i < j)
-		return (-15);
-	else if (i > j)
-		return (15);
-	else
-		return (0);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to be evaluated
- * Return: length of string
- */
-int _strlen(char *s)
-{
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-
-	return (i);
+	return (s1[i] - s2[i]);
 }
