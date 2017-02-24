1ec7f1980fca240e22d0465883e10e41f895b9cc
Carrie Ybay <carrieybay@gmail.com>
2017-02-23 19:51:52 -0800

    Edit 7-leet.c

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * leet - encodes a string into 1337
  * @s: string to be encoded
@@ -7,7 +5,7 @@
  */
 char *leet(char *s)
 {
-	int i, j = 0;
+	int i, j;
 	char l[] = {'a', 'e', 'o', 't', 'l'};
 	char L[] = {'A', 'E', 'O', 'T', 'L'};
 	char n[] = {'4', '3', '0', '7', '1'};
@@ -17,9 +15,7 @@ char *leet(char *s)
 		for (j = 0; j < 5; j++)
 		{
 			if (s[i] == l[j] || s[i] == L[j])
-			{
 				s[i] = n[j];
-			}
 		}
 	}
 	return (s);
