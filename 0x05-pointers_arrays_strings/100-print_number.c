2ed87edcb0f0c1f090f85938d01dffd79c111ede
Carrie Ybay <carrieybay@gmail.com>
2017-02-24 16:21:23 -0800

    Add 100-print_number.c and edit holberton.h

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	int num;
+	int last = n % 10;
+	int dig;
+	int exp = 1;
+
+        n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			dig = num / exp;
+			_putchar(dig + '0');
+			num = num - (dig * exp);
+			exp = exp / 10;
+		}
+	}
+	_putchar(last + '0');
+}
