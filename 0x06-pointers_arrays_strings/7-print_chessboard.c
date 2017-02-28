c5482061a43f12027a9fffee58e98bc7aaae6beb
Carrie Ybay <carrieybay@gmail.com>
2017-02-27 19:49:52 -0800

    Add 7-print_chessboard.c and edit holberton.h

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: pointer to array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
