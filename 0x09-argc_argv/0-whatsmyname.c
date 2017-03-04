eb3d1e21ba588df741727c28301c49ef58f14c82
Carrie Ybay <carrieybay@gmail.com>
2017-03-03 16:12:36 -0800

    Add 0-whatsmyname.c and _putchar.c

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
