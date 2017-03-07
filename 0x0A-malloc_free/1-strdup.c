6b9570eaf48db18913cb2a4da4765354a6ce3c44
Carrie Ybay <carrieybay@gmail.com>
2017-03-07 13:02:58 -0800

    Edit 1-strdup.c and holberton.h

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -12,18 +12,35 @@ char *_strdup(char *str)
 {
 	int i;
 	char *ar;
+	int len = _strlen(str);
 
 	if (str == NULL)
 		return (NULL);
 
-	ar = malloc(sizeof(char));
+	ar = malloc(len * sizeof(*str));
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		ar[i] = str[i];
 	}
-
 	ar[i] = '\0';
 
 	return (ar);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
