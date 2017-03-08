67c325a8ead7e7c8d8b60ccaa916e23503de992e
Carrie Ybay <carrieybay@gmail.com>
2017-03-08 13:46:11 -0800

    Edit 2-str_concat.c

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -16,12 +16,18 @@ char *str_concat(char *s1, char *s2)
 	int len, len1, len2;
 
 	if (s1 == NULL)
+	{
 		len1 = 0;
+		s1 = "";
+	}
 	else
 		len1 = _strlen(s1);
 
 	if (s2 == NULL)
+	{
 		len2 = 0;
+		s2 = "";
+	}
 	else
 		len2 = _strlen(s2);
 
