254beba45f38f39916fca6f9bf34fffe68b6adcb
Carrie Ybay <carrieybay@gmail.com>
2017-03-07 17:07:48 -0800

    Edit 2-str_concat.c

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -29,15 +29,14 @@ char *str_concat(char *s1, char *s2)
 
 	str = malloc((len * sizeof(char)) + 1);
 
+	if (str == NULL)
+		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		str[i] = s1[i];
-	}
 
 	for (; s2[j] != '\0'; i++, j++)
-	{
 		str[i] = s2[j];
-	}
 	str[i] = '\0';
 
 	return (str);
