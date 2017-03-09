5087236fa7703a132394dc4f96dac433af59e1c2
Carrie Ybay <carrieybay@gmail.com>
2017-03-09 13:35:45 -0800

    Add 1-string_nconcat.c and edit holberton.h

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of second string to use
+ * Return: pointer to allocated space in memory containing the concatenated
+ * strings or NULL if function fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1, len2;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (len2 > n)
+		len2 = n;
+
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j < len2; i++, j++)
+		str[i] = s2[j];
+
+	str[i] = '\0';
+
+	return (str);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -2,6 +2,8 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
+int _strlen(char *s);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /* HOLBERTON_H */
