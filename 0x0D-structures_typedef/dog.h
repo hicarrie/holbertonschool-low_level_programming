5e1f7513ff5455ef053adceca0d88108c7f5b66c
Carrie Ybay <carrieybay@gmail.com>
2017-03-13 15:05:02 -0700

    Add dog.h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data struct for dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
