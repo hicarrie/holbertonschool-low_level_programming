8007575419653ff0d8fc89449e7aef7feb739597
Carrie Ybay <carrieybay@gmail.com>
2017-03-13 23:12:06 -0700

    Edit 4-new_dog.c

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -21,6 +21,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
+	if (dog->name == NULL)
+	        dog->name = "(nil)";
+
+	if (dog->owner == NULL)
+	        dog->owner = "(nil)";
+
 	len_name = _strlen(name);
 	len_owner = _strlen(owner);
 
