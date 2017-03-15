3004b1d965ead9d7d3b65e3fb3e998a7f285a7a1
Carrie Ybay <carrieybay@gmail.com>
2017-03-15 13:36:35 -0700

    Add 0-print_name.c, _putchar.c, and function_pointers.h

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to function to print name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_HEADERS_H
+#define FUNCTION_HEADERS_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_HEADERS_H */
