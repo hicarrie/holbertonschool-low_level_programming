f9b13314c966f67fde81d2d454cac9d38e30de95
Carrie Ybay <carrieybay@gmail.com>
2017-03-16 13:50:13 -0700

    Edit 3-main.c for formatting

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -4,18 +4,22 @@
 
 /**
  * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: pointer to array of argument strings
  * Return: void
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
+	int a, b;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+        a = atoi(argv[1]);
+        b = atoi(argv[3]);
 
 	if (get_op_func(argv[2]) == NULL)
 	{
