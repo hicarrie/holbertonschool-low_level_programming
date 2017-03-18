cf2a96bc8ab64acecbb7398f4010f9922d5aab98
Carrie Ybay <carrieybay@gmail.com>
2017-03-18 13:13:18 -0700

    Edit 2-print_strings.c to remove unnecessary printed space

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -29,8 +29,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 			if (separator != NULL)
 				printf("%s", separator);
-			else
-				printf(" ");
 		}
 		else
 			printf("%s\n", s);
