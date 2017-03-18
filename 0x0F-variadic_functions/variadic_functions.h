d760b489382e6d1cd2fdd6164d1f646e9b8dc2bf
Carrie Ybay <carrieybay@gmail.com>
2017-03-18 14:46:52 -0700

    Edit variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -16,8 +16,7 @@ typedef struct print
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_strings(cononst format, ...);
 void print_c(va_list arg);
 void print_i(va_list arg);
 void print_f(va_list arg);
