c936ff188c99a9d7f8c1bc16466f978c37f1ea73
Carrie Ybay <carrieybay@gmail.com>
2017-03-18 13:27:05 -0700

    Edit 2-print_strings.c to fix problem with printing new line

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -31,8 +31,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 		}
 		else
-			printf("%s\n", s);
+			printf("%s", s);
 	}
+	printf("\n");
 
 	va_end(strings);
 }
