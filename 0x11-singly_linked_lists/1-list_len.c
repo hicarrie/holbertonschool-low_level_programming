eee4b8a7df972891c73dee3028051cef0c47fba1
Carrie Ybay <carrieybay@gmail.com>
2017-03-23 14:04:25 -0700

    Initial commit

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: list to evaluate
+ * Return: number of elements in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *current = h;
+	unsigned int n; /* counter for number of elements */
+
+	if (current == NULL)
+		return (1);
+
+	n = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		n++;
+	}
+	return (n);
+}
