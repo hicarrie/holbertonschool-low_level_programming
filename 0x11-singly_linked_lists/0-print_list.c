96e06e1e64ad32b40794e2e4647f998921a7d9f4
Carrie Ybay <carrieybay@gmail.com>
2017-03-23 13:41:04 -0700

    Initial commit

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: list to be printed
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	unsigned int n; /* number of nodes */
+
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%s\n", current->str);
+		current = current->next;
+		n++;
+	}
+
+	return (n);
+}
