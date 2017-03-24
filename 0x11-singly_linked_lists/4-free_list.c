0b2b49d6916bde853e9dc06b942d8ed746c1571c
Carrie Ybay <carrieybay@gmail.com>
2017-03-24 08:55:55 -0700

    Edit to free str before freeing node

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: list to be freed
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
