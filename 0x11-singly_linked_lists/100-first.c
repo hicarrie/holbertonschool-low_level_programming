52b3e93e0ca50fa2b646fa3842547e9dd8c69863
Carrie Ybay <carrieybay@gmail.com>
2017-03-24 12:58:02 -0700

    Initial commit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_before - prints a string before main function is executed
+ * Return: void
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
+}
