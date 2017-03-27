4295a66444cdcb2296dfaeac0e3dc5b35394ae44
Carrie Ybay <carrieybay@gmail.com>
2017-03-27 15:42:56 -0700

    Initial commit

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list and sets head to NULL
+ * @head: pointer to pointer to list to be freed
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = current->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
