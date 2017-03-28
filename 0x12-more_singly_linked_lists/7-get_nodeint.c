e2316d539b6edb0d826238a82db061ae5f9ed5fe
Carrie Ybay <carrieybay@gmail.com>
2017-03-28 14:00:39 -0700

    Edit to account for edge case

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+
 	current = head;
 
 	for (i = 0; i < index; i++)
-	{
 		current = current->next;
-	}
+
 	if (current == NULL)
 		return (NULL);
 
