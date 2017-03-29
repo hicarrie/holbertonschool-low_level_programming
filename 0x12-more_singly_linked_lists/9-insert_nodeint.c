ad19d023a24896c704e4cc0a795f8fbd1e80cc3e
Carrie Ybay <carrieybay@gmail.com>
2017-03-29 12:18:54 -0700

    Edit to stop infinite looping

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	current = *head;
+
 	if (current == NULL)
 		return (NULL);
 
@@ -29,17 +30,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	if (idx == i + 1)
-	{
-		new->next = NULL;
-		current->next = new;
-	}
-
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-	}
 
 	if (idx != 0)
 	{
@@ -48,6 +38,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		for (i = 0; i < idx - 1; i++)
 			current = current->next;
 		current->next = new;
+	}
+	if (idx == i + 1)
+	{
+		new->next = NULL;
+		current->next = new;
+	}
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
 	}
 	return (new);
 }
