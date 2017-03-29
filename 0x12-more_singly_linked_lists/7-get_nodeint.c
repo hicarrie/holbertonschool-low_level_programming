6119fc06e86c981bab2259447db19ea5674cdb33
Carrie Ybay <carrieybay@gmail.com>
2017-03-28 19:52:52 -0700

    Edit to check for edge case

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -12,21 +12,19 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 	unsigned int i, j;
 
-	if (head == NULL)
-		return (NULL);
-
 	current = head;
 
-	for (j = 0; current != NULL; j++)
-		current = current->next; /* count number of nodes in list */
+	for (j = 0; head != NULL; j++)
+		head = head->next; /* count number of nodes in list */
 
 	if (index > j)
-		exit (0); /* if index is bigger than list, exit */
-
-	current = head; /* resets current to head */
+		return (NULL); /* if index is bigger than list, return NULL */
 
 	for (i = 0; i < index; i++)
+	{
+		if (current == NULL)
+			return (NULL);
 		current = current->next; /* iterate to specified node */
-
+	}
 	return (current);
 }
