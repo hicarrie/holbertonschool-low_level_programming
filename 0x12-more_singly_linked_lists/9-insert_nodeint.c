82b83d815a6cf7fac949b39f4268ad6fb7c3de3a
Carrie Ybay <carrieybay@gmail.com>
2017-03-29 12:20:48 -0700

    Edit to properly account for edge cases

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,8 +30,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-
-	if (idx != 0)
+	if (idx == i + 1)
+	{
+		new->next = NULL;
+		current->next = new;
+	}
+	else if (idx != 0)
 	{
 		current = *head;
 		new->next = current->next;
@@ -39,15 +43,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current = current->next;
 		current->next = new;
 	}
-	if (idx == i + 1)
-	{
-		new->next = NULL;
-		current->next = new;
-	}
-	if (idx == 0)
+	else if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 	}
+
 	return (new);
 }
