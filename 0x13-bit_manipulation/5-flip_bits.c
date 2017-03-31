cdfef980b337e5246005f42a444196d2bd99f553
Carrie Ybay <carrieybay@gmail.com>
2017-03-31 10:10:54 -0700

    Initial commit

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip to get from
+ * one number to another
+ * @n: initial number
+ * @m: number to change to
+ * Return: number of bits needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int counter;
+	int size;
+
+	size = sizeof(unsigned long int) * 8 - 1;
+
+	counter = 0;
+	while (size != 0)
+	{
+		if ((n & 1) != (m & 1))
+			counter++;
+		n = n >> 1;
+		m = m >> 1;
+		size--;
+	}
+
+	return (counter);
+}
