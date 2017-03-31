d8cb63e49531f79222558889b2eca1cec950eb21
Carrie Ybay <carrieybay@gmail.com>
2017-03-30 20:51:31 -0700

    Initial commit

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets value of bit to 1 at given index
+ * @n: number to be evaluated
+ * @index: index starting from 0 of bit to be set
+ * Return: 1 on success, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int mask;
+
+	if (index > sizeof(unsigned long int) * 8 - 1)
+		return (-1); /* checks if index is larger than # of positions */
+
+	mask = 1 << index; /* shifts 1 to the index position */
+
+	*n = *n | mask;
+
+	return (1);
+}
