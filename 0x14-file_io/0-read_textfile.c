58faa2badb292770082d2f8fb729884ef29687d6
Carrie Ybay <carrieybay@gmail.com>
2017-04-04 11:34:50 -0700

    Initial commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file to read and print
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters read and printed or 0 if the file cannot be
+ * open or read, filename is NULL, or write fails or does not return expected
+ * amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buffer[1024];
+	int bytes; /* number of bytes read by read syscall */
+	int i;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	bytes = read(fd, buffer, letters);
+	buffer[bytes] = '\0';
+
+	close(fd);
+
+	i = 0;
+	while (buffer[i] != '\0')
+	{
+		_putchar(buffer[i]);
+		i++;
+	}
+
+	return (bytes);
+}
