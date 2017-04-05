ce1d6aac10bb95740fa0d11ea5e6af06ca522525
Carrie Ybay <carrieybay@gmail.com>
2017-04-05 15:04:51 -0700

    Fix error handling for exit(98)

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-		exit(exit_error(99, file_from, fd_from));
+		exit(exit_error(98, file_from, fd_from));
 
 	fd_to = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_to == -1)
