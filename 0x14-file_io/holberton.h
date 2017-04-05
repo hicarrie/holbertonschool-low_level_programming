a5a8626577f7b7aea4ce0bb494ad9b6ce20051ae
Carrie Ybay <carrieybay@gmail.com>
2017-04-05 13:53:59 -0700

    Remove prototype for main

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -7,7 +7,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int main(int argc, char *argv[]);
 int exit_error(int num, char *str, int fd);
 
 #endif /* HOLBERTON_H */
