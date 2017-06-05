18ffc9bfbe59b5a83215e871dd28b6df50c4baad
Carrie Ybay <carrieybay@gmail.com>
2017-06-05 11:57:58 -0700

    Edit to properly count number of elements in hash table for printing

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -8,22 +8,32 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
+	unsigned int count;
 	unsigned int i;
 
 	if (ht == NULL)
-	        return;
+		return;
+
+	count = 0;
+	while (ht->array[count] != NULL)
+	{
+		current = ht->array[count];
+		while (current != NULL)
+			current = current->next;
+		count++;
+	}
 
 	printf("{");
 
 	i = 0;
-	while(i < ht->size)
+	while (i < ht->size)
 	{
 		current = ht->array[i];
 		while (current != NULL)
 		{
 			printf("'%s': '%s'", current->key, current->value);
 			current = current->next;
-			if (i != ht->size - 1)
+			if (i < count)
 				printf(", ");
 		}
 		i++;
