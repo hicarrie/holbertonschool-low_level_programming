e02ee5c868a4f1c8353ea2ec5fd72970cfff9a86
Carrie Ybay <carrieybay@gmail.com>
2017-06-04 21:00:16 -0700

    Add checks for NULL values

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	unsigned long int index;
 
-	if (key == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (EXIT_FAILURE);
 
 	index = key_index((const unsigned char *)key, ht->size);
